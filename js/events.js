23c3324606fe27c0a0e4847d7b19cf211889fa7f
Lalit <lalitkumarsingh3716@gmail.com>
2022-09-28 11:09:45 +0530

    Added Tab Switching Functionality

---
+++ js/events.js
@@ -0,0 +1,31 @@
+function controlsManager(controlsName) {
+  const controls = document.querySelectorAll(
+    `.${controlsName}-controls > button`
+  );
+  for (let i = 0; i < controls.length; i++) {
+    controls[i].addEventListener("click", function () {
+      const current = document.querySelector(
+        `.${controlsName}-controls > button.btn-success`
+      );
+      const content = this.innerText.toLowerCase();
+      const newDoc = document.querySelector(`.${controlsName} #${content}`);
+      newDoc.classList.remove("hide");
+      newDoc.classList.add("show");
+
+      const currentDoc = document.querySelector(
+        `.${controlsName} #${current.innerText.toLowerCase()}`
+      );
+      currentDoc.classList.remove("show");
+      currentDoc.classList.add("hide");
+
+      current.classList.remove("btn-success");
+      this.classList.add("btn-success");
+    });
+  }
+}
+
+controlsManager("goc");
+controlsManager("webify");
+controlsManager("shark");
+controlsManager("terror");
+controlsManager("tech-buzz");
